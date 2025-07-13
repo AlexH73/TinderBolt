0e82ecb4f16c964f69f8ecac80ee8866fd67f1e7
Alex_H <ewebotah@gmail.com>
2025-07-13 23:07:07 +0200

    Initial commit

---
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -0,0 +1,47 @@
+package com.javarush.telegram;
+
+import com.javarush.telegram.ChatGPTService;
+import com.javarush.telegram.DialogMode;
+import com.javarush.telegram.MultiSessionTelegramBot;
+import com.javarush.telegram.UserInfo;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.ArrayList;
+
+public class TinderBoltApp extends MultiSessionTelegramBot {
+    public static final String TELEGRAM_BOT_NAME = "alex_tinder_ai_bot";
+    public static final String TELEGRAM_BOT_TOKEN = "8112518354:AAGcbyZeMDVZF_1xTvNkmzGWaKFt2m8S9kg";
+    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+
+    public TinderBoltApp() {
+        super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        String message = getMessageText();
+
+        if (message.equals("/start")) {
+            sendPhotoMessage("main");
+            String text = loadMessage("main");
+            sendTextMessage(text);
+
+            return;
+        }
+        sendTextMessage("*Привет!*");
+        sendTextMessage("_Привет!_");
+        sendTextMessage("Вы написали " + message);
+
+        sendTextButtonsMessage("Выберите режим работы",
+                "Старт", "start",
+                "Стоп", "stop");
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TinderBoltApp());
+    }
+}
