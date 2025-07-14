33c31962ccd2076c5ef63b3d269840dd1c52a02a
Alex_H <ewebotah@gmail.com>
2025-07-14 21:35:04 +0200

    ЗАНЯТИЕ №2. Прикручиваем chatGPT и пишем каркас бота

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -14,8 +14,10 @@ import java.util.ArrayList;
 public class TinderBoltApp extends MultiSessionTelegramBot {
     public static final String TELEGRAM_BOT_NAME = "alex_tinder_ai_bot";
     public static final String TELEGRAM_BOT_TOKEN = "123456:ABC-DEF1234";
-    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+    public static final String OPEN_AI_TOKEN = "sk-1234567890";
 
+    private ChatGPTService chatGPT = new ChatGPTService(OPEN_AI_TOKEN);
+    private DialogMode currentMode = null;
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
     }
@@ -25,10 +27,32 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
         String message = getMessageText();
 
         if (message.equals("/start")) {
+            currentMode = DialogMode.MAIN;
             sendPhotoMessage("main");
             String text = loadMessage("main");
             sendTextMessage(text);
 
+            showMainMenu("Главное меню бота", "/start",
+                    "Генерация Tinder-профля \uD83D\uDE0E", "/profile",
+                    "Сообщение для знакомства \uD83E\uDD70", "/opener",
+                    "Переписка от вашего имени \uD83D\uDE08", "/message",
+                    "Переписка со звездами \uD83D\uDD25", "/date",
+                    "Задать вопрос чату GPT \uD83E\uDDE0", "/gpt");
+            return;
+        }
+
+        if (message.equals("/gpt")) {
+            currentMode = DialogMode.GPT;
+            sendPhotoMessage("gpt");
+            String text = loadMessage("gpt");
+            sendTextMessage(text);
+            return;
+        }
+
+        if (currentMode == DialogMode.GPT) {
+            String prompt = loadPrompt("gpt");
+            String answer = chatGPT.sendMessage(prompt, message);
+            sendTextMessage(answer);
             return;
         }
         sendTextMessage("*Привет!*");
